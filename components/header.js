35cc3b3f751e00d6971bd453894a1d5e5b6202c3
Ts3nguun <0805tsenguun@gmail.com>
2024-12-04 13:05:38 +0800

    real last change

--- components/header.js
+++ components/header.js
@@ -42,7 +42,6 @@ export function Navigation_bar() {
 
                 </div>
                 <div className="flex gap-6 items-center">
-                    <div className="hidden lg:block">Change theme</div>
                     <Link href={"Light.pdf"} target="blank">
                         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded hidden lg:block">
                             Download CV
